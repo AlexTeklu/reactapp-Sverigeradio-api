a627af292955400461cd92eed6885b83b61d65fe
AlexTeklu <alexanderosteklu@gmail.com>
2022-06-01 17:36:58 +0200

    app completed

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import './footer.css';
+import {BsFacebook, BsGithub, BsLinkedin} from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <footer>
     <a href="#/" className='footer_logo'>Alexander</a>
+    <div className="footer_socials">
+        <a href="https://linkedin.com" target="blank"> <BsLinkedin /> </a>
+        <a href="https://github.com/AlexTeklu/reactapp-radio-api" target="blank"> <BsGithub /> </a>
+        <a href="https://facebook.com" target="blank"> <BsFacebook /> </a>
+    </div>
  
     <div className="footer_copyright">
       <small>&copy; Alexander Teklu. all rights reserved</small>
